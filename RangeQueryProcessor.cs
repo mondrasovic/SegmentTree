b31ace5f4550aa3fdd8692939619ffbe4cbdaa28
Nightwalker495 <milan.ondrasovic@gmail.com>
2018-09-24 07:47:08 +0200

    Implement range query mechanism.

--- RangeQueryProcessor.cs
+++ RangeQueryProcessor.cs
@@ -38,7 +38,7 @@ namespace SegmentTree
 
         public T QueryRange(int startIndex, int endIndex)
         {
-            throw new NotImplementedException();
+            return QueryRange(startIndex, endIndex, 0, baseArray.Length - 1, 0);
         }
 
         static int CalcSegTreeArraySize(int underlyingArraySize)
@@ -84,6 +84,50 @@ namespace SegmentTree
             var rightVal = segTree[segTreeRightChildIndex];
             var accumulatedVal = accumulator.Invoke(leftVal, rightVal);
             segTree[segTreeRootIndex] = accumulatedVal;
+        }
+
+        T QueryRange(int queryStartIndex, int queryEndIndex,
+                     int baseArrayStartIndex, int baseArrayEndIndex,
+                     int segTreeRootIndex)
+        {
+            if (IsTotalOverlapCase(queryStartIndex, queryEndIndex,
+                                   baseArrayStartIndex, baseArrayEndIndex))
+            {
+                return segTree[segTreeRootIndex];
+            }
+
+            if (IsNoOverlapCase(queryStartIndex, queryEndIndex,
+                                baseArrayStartIndex, baseArrayEndIndex))
+            {
+                return placeholder;
+            }
+
+            var baseArrayMidIndex = (baseArrayStartIndex + baseArrayEndIndex)
+                / 2;
+            var segTreeLeftChildIndex = (segTreeRootIndex * 2) + 1;
+            var segTreeRightChildIndex = (segTreeRootIndex * 2) + 2;
+
+            var leftVal = QueryRange(queryStartIndex, queryEndIndex,
+                                     baseArrayStartIndex, baseArrayMidIndex,
+                                     segTreeLeftChildIndex);
+            var rightVal = QueryRange(queryStartIndex, queryEndIndex,
+                                      baseArrayMidIndex + 1, baseArrayEndIndex,
+                                      segTreeRightChildIndex);
+            return accumulator.Invoke(leftVal, rightVal);
+        }
+
+        bool IsTotalOverlapCase(int queryStartIndex, int queryEndIndex,
+                                int baseArrayStartIndex, int baseArrayEndIndex)
+        {
+            return (queryStartIndex <= baseArrayStartIndex) &&
+                (queryEndIndex >= baseArrayEndIndex);
+        }
+
+        bool IsNoOverlapCase(int queryStartIndex, int queryEndIndex,
+                             int baseArrayStartIndex, int baseArrayEndIndex)
+        {
+            return (queryEndIndex < baseArrayStartIndex) ||
+                (queryStartIndex > baseArrayEndIndex);
         }
 
         void UpdateValue(int index, T value)
